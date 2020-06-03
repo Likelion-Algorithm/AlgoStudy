40d46799745e8a24528e06ae54986c491cf84725
rkdrud34 <rkdrud34@nate.com>
2020-06-04 02:57:44 +0900

    회전하는큐

---
+++ 1021_회전하는 큐.py
@@ -0,0 +1,32 @@
+import sys
+num_input = list(map(int,sys.stdin.readline().strip().split()))
+
+num_list = [i+1 for i in range(num_input[0])]
+num_index = num_input[1]
+delete_list = list(map(int,sys.stdin.readline().strip().split()))
+# 출력값 변수 선언
+move_index =0
+while delete_list:
+    if num_list[0] == delete_list[0]:
+        # 원하는 값이 맨 앞에 있을때
+        del delete_list[0], num_list[0]
+    elif num_list.index(delete_list[0]) <= len(num_list)//2:
+        # 지워야할 수가 num_list 중앙보다 왼쪽에 있거나 중앙에 있을때
+        num_list.append(num_list[0]) 
+        del num_list[0]
+        move_index+=1
+    elif num_list.index(delete_list[0]) > len(num_list)//2:
+        # 지워야할 수가 num_list 중앙보다 오른쪽에 있을 때
+        num_list.insert(0,num_list[-1])
+        del num_list[-1]
+        move_index+=1
+
+
+print(move_index)
+    
+# # num_list = [[1, 0], [4, 2], [6, 0]]
+# # weight_list= [[5], [1, 2, 3, 4], [1, 1, 9, 1, 1, 1]]
+# num_list,weight_list  = list(),list()
+# for i in range(num_input):
+#     num_list.append(list(map(int,sys.stdin.readline().strip().split())))
+#     weight_list.append(list(map(int,sys.stdin.readline().strip().split())))
--- 연습장.py
+++ 연습장.py
@@ -21,37 +21,41 @@ if '21 Junkyu' < '21 Dohyun':
 if '21 Junkyu' == '21 Dohyun':
     print('hooo')
 
-arr1 = [1,5,7,2,9,13,10,13,7]
-arr2 = [2,3,9,10,4,8,11,11]
-arr_1_sort={}
-arr_2_sort={}
-def solution(arr1,arr2):
-    for i in arr1:
-        for j in arr1:
-            if i == j:
-                if j not in arr_1_sort.keys():
-                    arr_1_sort[j] = 1
-                elif j in arr_1_sort.keys():
-                    arr_1_sort[j] += 1
-    for i in arr2:
-        for j in arr2:
-            if i == j:
-                if j not in arr_2_sort.keys():
-                    arr_2_sort[j] = 1
-                elif j in arr_2_sort.keys():
-                    arr_2_sort[j] += 1
+# arr1 = [1,5,7,2,9,13,10,13,7]
+# arr2 = [2,3,9,10,4,8,11,11]
+# arr_1_sort={}
+# arr_2_sort={}
+# def solution(arr1,arr2):
+#     for i in arr1:
+#         for j in arr1:
+#             if i == j:
+#                 if j not in arr_1_sort.keys():
+#                     arr_1_sort[j] = 1
+#                 elif j in arr_1_sort.keys():
+#                     arr_1_sort[j] += 1
+#     for i in arr2:
+#         for j in arr2:
+#             if i == j:
+#                 if j not in arr_2_sort.keys():
+#                     arr_2_sort[j] = 1
+#                 elif j in arr_2_sort.keys():
+#                     arr_2_sort[j] += 1
             
 
-solution(arr1,arr2)
-if max(arr_1_sort.values())==1 and max(arr_1_sort.values())==1:
-    print(0)
-elif max(arr_1_sort.values()) < max(arr_1_sort.values()):
-    print(2)
-elif max(arr_1_sort.values()) > max(arr_1_sort.values()):
-    print(1)
-# elif max(arr_1_sort.values()) == max(arr_1_sort.values()):
+# solution(arr1,arr2)
+# if max(arr_1_sort.values())==1 and max(arr_1_sort.values())==1:
+#     print(0)
+# elif max(arr_1_sort.values()) < max(arr_1_sort.values()):
+#     print(2)
+# elif max(arr_1_sort.values()) > max(arr_1_sort.values()):
+#     print(1)
+# # elif max(arr_1_sort.values()) == max(arr_1_sort.values()):
 
 
-print(arr_1_sort)
-print(arr_2_sort)
-print(max(arr_1_sort))
+# print(arr_1_sort)
+# print(arr_2_sort)
+# print(max(arr_1_sort))
+
+
+a = [1,2,3,4,5,6]
+print(len(a)//2)
