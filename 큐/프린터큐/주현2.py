d91d5efe4398aa60d5d68191fc63520934537261
gomiseki <kimju1755@gmail.com>
2020-06-04 02:17:23 +0900

    주현 제로 힐링

--- 큐/프린터큐/주현2.py
+++ 큐/프린터큐/주현2.py
@@ -1,31 +1,30 @@
 class PrintQueue:
-
     def __init__(self,docQty,docLoc,importance):
         self.docQty = docQty
         self.docLoc = docLoc
         self.importance = importance
+        self.order=0
+        self.front=0
+        self.rear=len(self.importance)
         
 
     def getOrder(self):
-        order=1
-        while(self.importance[0] != self.importance[self.docLoc]):
-            for i in range(0, len(self.importance)):
-                if(self.importance[0]<self.importance[i]):
-                    self.enQueue(self.deQueue())
-        
-            self.deQueue()
-            order += 1
-        return order
+        i=self.front
+            while(self.importance[i]<self.importance[self.front]):
+                i+=1
+                if(i==self.rear):
+                    self.deQueue()
+            self.enQueue(self.deQueue())
 
     def deQueue(self):
-        if self.importance[self.docLoc]!=self.importance[0]:
-            self.docLoc-=1
-        return self.importance.pop(0)
+        temp=self.importance[self.front]
+        self.importance[self.front]=0
+        self.front+=1
+        return temp
         
     def enQueue(self,data):
         self.importance.append(data)
-        if data==self.importance[self.docLoc]:
-            self.docLoc=len(self.importance)-1
+        self.rear+=1
 
     
 testcase = int(input())
