13ec79c515e503c30e9505d110fe8965a6d40c59
rkdrud34 <rkdrud34@nate.com>
2020-06-05 21:44:53 +0900

    d

--- 연습장.py
+++ 연습장.py
@@ -59,3 +59,37 @@ if '21 Junkyu' == '21 Dohyun':
 
 a = [1,2,3,4,5,6]
 print(len(a)//2)
+
+# import sys
+# num_input = int(sys.stdin.readline())
+# num_list,weight_list  = list(),list()
+# for i in range(num_input):
+#     num_list.append(list(map(int,sys.stdin.readline().strip().split())))
+#     weight_list.append(list(map(int,sys.stdin.readline().strip().split())))
+    
+num_list = [[1, 0], [4, 2], [6, 0]]
+weight_list= [[5], [1, 2, 3, 4], [1, 1, 9, 1, 1, 1]]
+
+def function(num_list, weight_list):
+    
+    print_index = 1
+    num_list_index = num_list[1]
+    while weight_list:
+        print(weight_list,num_list,num_list_index)
+        if max(weight_list) == weight_list[0]:
+            if num_list_index == 0:
+                print(print_index)
+                break
+            del weight_list[0]
+            print_index +=1
+            num_list_index-=1
+        if max(weight_list) > weight_list[0]:
+            weight_list.append(weight_list[0])
+            del weight_list[0]
+            if num_list_index == 0:
+                num_list_index = len(weight_list)-1
+            else:
+                num_list_index -=1
+        
+for i in range(len(num_list)):
+    function(num_list[i],weight_list[i])
