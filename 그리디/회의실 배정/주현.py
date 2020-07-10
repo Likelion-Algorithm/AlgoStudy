f2792a3b5937867d8dddc50479248c278f8a7042
gomiseki <kimju1755@gmail.com>
2020-07-10 19:03:47 +0900

    회의실배정 완성

--- 그리디/회의실 배정/주현.py
+++ 그리디/회의실 배정/주현.py
@@ -1,29 +1,15 @@
 N = int(input())
 timelist = list()
-timesub = list()
-rangelist = set()
-count = 0
-temp = 1
-tempmax = 0
-tempmin = 0
+count = 1
 for i in range(N):
     a,b = input().split()
     a,b = int(a),int(b)
-    if a < tempmin:
-        tempmin = a
-    if b > tempmax:
-        tempmax = b
-    timelist.append((a,b))
-    timesub.append(b-a)
-while (tempmax-tempmin)-len(rangelist)>min(timesub): #timesub시 시간초과
-    loc = timesub.index(min(timesub))
-    if (timelist[loc][1]-1 not in rangelist and timelist[loc][0]+1 not in rangelist):
-        i = range(timelist[loc][0],timelist[loc][1]+1)
-        rangelist.update(list(i))
+    timelist.append([a,b])
+
+timelist = sorted(timelist, key=lambda x:(x[1],x[0]))
+temp = timelist[0][1]
+for i in range(1,len(timelist)):
+    if(timelist[i][0]>=temp):
         count+=1
-    timelist.pop(loc)
-    timesub.pop(loc)
-
+        temp = timelist[i][1]
 print(count)
-
-    
