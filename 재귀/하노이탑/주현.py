270c9454ad93b370732aabf25a4d5dba10814008
gomiseki <kimju1755@gmail.com>
2020-07-15 14:24:51 +0900

    주현 하노이탑 풀이

--- 재귀/하노이탑/주현.py
+++ 재귀/하노이탑/주현.py
@@ -1,65 +1,35 @@
-def getCount(num):
-    tempmax=0
-    tempmin=0
-    hanoiBase1=list()
-    hanoiBase2=list()
-    hanoiBase3=list()
-    hanoiBase1.append(0)
-    hanoiBase2.append(0)
-    hanoiBase3.append(0)
-    Baselist=[hanoiBase1,hanoiBase2,hanoiBase3]
-    for block in range(num,0,-1):   #첫 번째 장대에 세팅
-        hanoiBase1.append(block)
-        count=0
-        Base=0
-    if num%2==0:
-        hanoiBase2.append(hanoiBase1.pop())
-        Base=2
-        count+=1
-    else:
-        hanoiBase3.append(hanoiBase1.pop())
-        Base=3
-        count+=1
-    if num==1:
-        return 1
-    else:
-        while hanoiBase3[-1]!=num:
-            for i in range(0,3):
-                if i==Base:
-                    continue
-                if Baselist[i][-1]>Baselist[tempmax][-1]:
-                    tempmax=i
-            for j in range(0,3):
-                if j==tempmax or Baselist[j][-1]>Baselist[tempmax][-1]:
-                    continue
-                if Baselist[j][-1]<=Baselist[tempmin][-1]:
-                    tempmin=j
-            if Baselist[tempmin][-1]==0:
-                Baselist[tempmin].append(Baselist[tempmax].pop())
-                Base=tempmin
-                count+=1
-                continue
-            elif(Baselist[tempmax][-1]==max(Baselist[0][-1],Baselist[1][-1],Baselist[2][-1])):
-                for k in range(0,3):
-                    if(k!=tempmax and k!=Base):
-                        tempmax=k
-                        if tempmin==tempmax:
-                            tempmin=(tempmin+1)%2
-                        break
-                for j in range(0,3):
-                    if j==tempmax or Baselist[j][-1]<Baselist[tempmax][-1]:
-                        continue
-                    else:
-                        tempmin=j
-            Baselist[tempmin].append(Baselist[tempmax].pop())
-            Base=tempmin
-            count+=1
-            continue
-
-        return count+getCount(num-1)
-    
-
+              
 num = int(input())
-print(getCount(num))
-    
-#아시발 못하겠다진짜
+movelist = list()
+templist = list()
+for i in range(6):
+    movelist.append([i])
+    templist.append([])
+
+for i in range(1,num):
+    for j in range(6):
+        if j==0 or j==1:
+            templist[j]= movelist[(j+1)%2]+[j]+movelist[5-(j*2)]
+        elif j==2 or j==3:
+            templist[j]= movelist[2+(j+1)%2]+[j]+movelist[10%(j*3)]
+        else:
+            templist[j]= movelist[4+(j+1)%2]+[j]+movelist[10%(j*2)]
+    movelist=templist[:]
+
+print(len(movelist[1]))
+for i in range(len(movelist[1])):
+    if movelist[1][i]==0:
+        print('1','2')
+    elif movelist[1][i]==1:
+        print('1','3')
+    elif movelist[1][i]==2:
+        print('2','1')
+    elif movelist[1][i]==3:
+        print('2','3')
+    elif movelist[1][i]==4:
+        print('3','1')
+    elif movelist[1][i]==5:
+        print('3','2')
+
+#동적계획법 만세
+#푸는데 2주걸림
