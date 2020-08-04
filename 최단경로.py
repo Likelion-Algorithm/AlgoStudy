7d76bea2056ba9b86325d8f1167cb71041a4d241
rkdrud34 <rkdrud34@nate.com>
2020-08-05 05:30:24 +0900

    런타임에러ㅜㅜㅜㅜㅜ

--- 최단경로.py
+++ 최단경로.py
@@ -8,8 +8,37 @@ for i in range(spot):
 for i in range(num):
     start,finish,weight = list(map(int,sys.stdin.readline().split()))
     all_dict[start].append([finish,weight])
-print(all_dict)
-    
+# print(all_dict)
+# print(exp_list)
+
+
+
+
+def func(begin):
+    # print(exp_list)
+    for i in all_dict[begin]:
+        if i[0] == source:
+            return
+        if exp_list[i[0]]!=0:
+            exp_list[i[0]] = min(exp_list[i[0]], i[1]+exp_list[begin])
+        else:
+            exp_list[i[0]]=i[1]+exp_list[begin]
+        func(i[0])    
+func(source)
+
+for i in range(len(exp_list)):
+    if i == 0 :
+        continue
+    elif i == source:
+        print(0)
+    elif exp_list[i] == 0:
+        print("INF")
+    else:
+        print(exp_list[i])
+# for i in range(len(exp_list)):
+
+
+
 # 여기부터는 1부터 정점 마다의 최단 경로
 # def func(begin):
 #     print(exp_list)
