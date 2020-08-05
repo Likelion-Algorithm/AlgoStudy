95e7a78cf948a142586379015a9568a3ba06386f
gomiseki <kimju1755@gmail.com>
2020-08-05 20:34:33 +0900

    주현 최단경로 풀이 완성

--- 그래프/최단경로/주현.py
+++ 그래프/최단경로/주현.py
@@ -1,31 +1,28 @@
 import heapq,sys
+
 V, E = map(int, sys.stdin.readline().strip().split())
 start = int(sys.stdin.readline())
-graph = {}
+graph = {i:[] for i in range(1, V+1)}
 pathList = list()
 queue = list()
 INF = V*11
 for i in range(V+1):
     pathList.append(INF)
 pathList[start]=0
-for i in range(1,V+1):
-    graph[i] = []
 for i in range(E):
     u,v,w = map(int,sys.stdin.readline().strip().split())
     graph[u].append([w,v])
 heapq.heappush(queue, [0,start])
 while queue:
-    loc = heapq.heappop(queue)
-    for i in range(len(graph[loc[1]])):
-        heapq.heappush(queue, graph[loc[1]][i])
-        if pathList[graph[loc[1]][i][1]]>pathList[loc[1]]+graph[loc[1]][i][0]:
-            pathList[graph[loc[1]][i][1]] = pathList[loc[1]]+graph[loc[1]][i][0]
-
+    loc = heapq.heappop(queue)[1]
+    for w, v in graph[loc]:
+        if pathList[v]>pathList[loc]+w:
+            pathList[v] = pathList[loc]+w
+            heapq.heappush(queue, [pathList[v],v])
     
     
-for i in range(1,len(pathList)):
+for i in range(1,V+1):
     if pathList[i]==INF:
         print('INF')
     else:
         print(pathList[i])
-
