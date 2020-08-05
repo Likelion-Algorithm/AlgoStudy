5110aad8cc6d3ecde8b45982ac78c2adba918361
gomiseki <kimju1755@gmail.com>
2020-08-05 20:11:38 +0900

    주현 최단경로 2

--- 그래프/최단경로/주현.py
+++ 그래프/최단경로/주현.py
@@ -8,17 +8,18 @@ INF = V*11
 for i in range(V+1):
     pathList.append(INF)
 pathList[start]=0
+for i in range(1,V+1):
+    graph[i] = []
 for i in range(E):
     u,v,w = map(int,sys.stdin.readline().strip().split())
-    graph[u]=[w,v]
-heapq.heappush(queue, graph[start])
+    graph[u].append([w,v])
+heapq.heappush(queue, [0,start])
 while queue:
     loc = heapq.heappop(queue)
     for i in range(len(graph[loc[1]])):
         heapq.heappush(queue, graph[loc[1]][i])
-        if pathList[graph[loc[1]][i][1]]>pathList[loc[1]]+loc[0]:
-            pathList[graph[loc[1]][i][1]] = pathList[loc[1]]+loc[0]
-print(pathList)
+        if pathList[graph[loc[1]][i][1]]>pathList[loc[1]]+graph[loc[1]][i][0]:
+            pathList[graph[loc[1]][i][1]] = pathList[loc[1]]+graph[loc[1]][i][0]
 
     
     
