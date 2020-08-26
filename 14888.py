0502b3b9f1e3094c070c2983bc8a67dc5ffad1d2
ilmunkoo <mun90505@naver.com>
2020-08-26 18:54:01 +0900

    연산자 끼워넣기

---
+++ 14888.py
@@ -0,0 +1,28 @@
+from itertools import combinations
+import sys
+
+N = int(sys.stdin.readline().strip())
+numbers = list(map(int, sys.stdin.readline().split()))
+operators = ['+', '-', 'x', '/']
+operator_list = list(map(int, sys.stdin.readline().split()))
+
+converted = [[1]*operator_list[0], [2]*operator_list[1], [3]*operator_list[2], [4]*operator_list[3]]
+converted_list = []
+
+for i in range(len(operator_list)): #연산자 모으는 for문
+    for j in range(1,i+1):
+        converted_list.append()
+
+print(converted_list)
+numbers_combination = list(combinations(numbers,2))
+
+def operation(operator,a, b):
+    if operator == '+':
+        return a + b
+    elif operator == '-':
+        return a - b
+    elif operator == 'x':
+        return a*b
+    else:
+        return a // b
+
