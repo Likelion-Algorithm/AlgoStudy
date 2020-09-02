160632a11c3b69868fc2ba4e37eeebb2c7b82b04
Koo <mun90505@naver.com>
2020-09-02 17:39:08 +0900

    연산자 끼워넣기

--- 14888.py
+++ 14888.py
@@ -9,12 +9,14 @@ operator_list = list(map(int, sys.stdin.readline().split()))
 converted = [[1]*operator_list[0], [2]*operator_list[1], [3]*operator_list[2], [4]*operator_list[3]]
 converted_list = []
 
+
 for i in range(len(operator_list)): #연산자 모으는 for문
     for j in range(1,i+1):
         converted_list.append()
 
-print(converted_list)
+
 numbers_combination = list(combinations(numbers,2))
+
 
 def operation(operator,a, b):
     if operator == '+':
