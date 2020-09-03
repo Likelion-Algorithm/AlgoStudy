7d1ff603e0f05b1af050ce76dc78d283c5948bd0
Juhyeon <kimju1755@gmail.com>
2020-09-03 14:31:22 +0900

    lcs수정

--- 동적 계획법/lcs/주현.py
+++ 동적 계획법/lcs/주현.py
@@ -1,6 +1,6 @@
 first = input()
 second = input()
-dpList = [[0 for col in range(len(first)+1)] for row in range(len(second)+1)]
+dpList = [[0 for col in range(len(second)+1)] for row in range(len(first)+1)]
 for i in range(1,len(first)+1):
     for j in range(1,len(second)+1):
         if first[i-1]==second[j-1] :
