8c1a842e0d52b39b1f4ae1cdb8f3a04dce771183
Koo <mun90505@naver.com>
2020-09-09 17:42:27 +0900

    일문

--- 동적 계획법/가장 긴 증가하는 부분 수열/일문.py
+++ 동적 계획법/가장 긴 증가하는 부분 수열/일문.py
@@ -1,4 +1,29 @@
 import sys
+N = int(sys.stdin.readline().strip())
+sequence = list(map(int, sys.stdin.readline().split()))
+sol_list = list()
+len_list = list()
+for i in range(N):
+    sol_list.append(sequence[i])
+    for j in range(N):
+        if i >= j :
+            continue
+        else:
+            if sequence[j] <= sol_list[len(sol_list)-1]:
+                continue
+            else:
+                sol_list.append(sequence[j])
+    len_list.append(len(sol_list))
+    sol_list.clear()
+print(max(len_list))
+
+# 다음 반례를 풀지 못해 틀림
+# 즉, 첫 1, 4,5를 잡아낸 후 다음 2 3 4 5를 잡아내지 못하는 문제점이 있었다.
+# 7
+# 1 4 5 2 3 4 5
+
+
+import sys
 n = int(sys.stdin.readline().strip())
 seqeunce = list(map(int, sys.stdin.readline().split()))
 dp = [0 for i in range(n)]
