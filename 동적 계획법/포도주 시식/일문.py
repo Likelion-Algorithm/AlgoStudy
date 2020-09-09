b566ae215d4367f4d69caa9dcfb11a434bff59c4
IlmunKoo <59404684+IlmunKoo@users.noreply.github.com>
2020-09-09 17:47:02 +0900

    Update 일문.py

--- 동적 계획법/포도주 시식/일문.py
+++ 동적 계획법/포도주 시식/일문.py
@@ -14,6 +14,6 @@ for i in range(1,N+1):
     elif i == 2:
         sol_list[2] =  wine_list[1] + wine_list[2]
 
-    else: sol_list[i] = max(sol_list[i-3] + wine_list[i-1]+ wine_list[i], sol_list[i-2] + wine_list[i], wine_list[i-1])
+    else: sol_list[i] = max(sol_list[i-3] + wine_list[i-1]+ wine_list[i], sol_list[i-2] + wine_list[i], sol_list[i-1])
 
 print(sol_list[N])
