56d314fbd10d3d8fe7f5b3fe48fb2aca653cf523
rkdrud34 <rkdrud34@nate.com>
2020-09-16 19:03:59 +0900

    경욱

--- 동적 계획법/포도주 시식/경욱.py
+++ 동적 계획법/포도주 시식/경욱.py
@@ -7,3 +7,6 @@ for i in range(num):
 for i in range(3,len(result_list)):
     result_list[i] = max(result_list[i-1],result_list[i-2]+check_list[i], result_list[i-3]+check_list[i-1]+check_list[i])
 print(max(result_list))
+
+
+
