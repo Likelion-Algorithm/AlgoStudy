c899b212ec1a17942b450df04d968f3daeacc94b
gomiseki <kimju1755@gmail.com>
2020-11-04 15:19:10 +0900

    주현 그리디 풀이추가

---
+++ 그리디/큰 수 만들기/주현.py
@@ -0,0 +1,29 @@
+from collections import deque
+def solution(number, k):
+    answer = ''
+    number = list(map(int,number))
+    length = len(number)
+    number = deque(number)
+    while k!=0 and length!=k:
+        n = 0
+        for i in range(k+1):    #n= number.index(max(number[0:k+1]))
+            if number[i]==9:    #무작정 max보다는 9일때 빠르게 끝내는게 속도에 도움이됨
+                n=i
+                break
+            elif number[i]>number[n]:
+                n=i
+
+        for i in range(n):      #number=number[n:]
+            number.popleft()    #O(1)로O(n)인리스트 복사보다 빠름
+            length-=1
+        answer+=str(number.popleft())
+        length-=1
+        k-=n
+    if len(number)==k:
+        return answer
+    else:
+        for i in number:
+            answer+=str(i)
+    return answer
+
+print(solution('21',1))
