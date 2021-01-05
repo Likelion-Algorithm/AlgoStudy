54c8435cc5a6298934ba0958f8672c779c97cd93
JungTag <dbsdudxor121@naver.com>
2021-01-06 02:22:29 +0900

    영택 풀이추가

--- 티어 별 문제/실버1/RGB거리/영택.py
+++ 티어 별 문제/실버1/RGB거리/영택.py
@@ -2,17 +2,18 @@ import sys
 
 N = int(input())
 arr = []
+R, G, B = 0, 1, 2
 
 for _ in range(N):
     costs = [int(x) for x in sys.stdin.readline().split()]
     arr.append(costs)
 
 dp = [[0]*3 for _ in range(N)]
-dp[0][0], dp[0][1], dp[0][2] = arr[0][0], arr[0][1], arr[0][2]
+dp[0][R], dp[0][G], dp[0][B] = arr[0][R], arr[0][G], arr[0][B]
 
 for i in range(1, N):
-    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + arr[i][0]
-    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + arr[i][1]
-    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + arr[i][2]
+    dp[i][R] = min(dp[i-1][G], dp[i-1][B]) + arr[i][R]
+    dp[i][G] = min(dp[i-1][R], dp[i-1][B]) + arr[i][G]
+    dp[i][B] = min(dp[i-1][R], dp[i-1][G]) + arr[i][B]
 
 print(min(dp[N-1]))
